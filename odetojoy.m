4e68dcd06ceea9f4a51e26d074c0f392c057c084
Loukas_Anastasiadis <78563425+AnastasiadisLoukas@users.noreply.github.com>
2022-12-08 19:40:16 +0200

    Add files via upload

---
+++ odetojoy.m
@@ -0,0 +1,14 @@
+function v = odetojoy
+% Solfege - Ode to joy
+%  Usage:
+%  pianoex(odetojoy)
+C = 0; D = 2; E = 4; F = 5; G = 7; A = 9; B = 11;
+e = 1/4; q = 2/5; h = 2/3; t = 3/2;
+
+v  = {E h; E h; F h; G h; G h; F h; E h; D h; C h; C h; D h; E h; E h; D q; D t; 
+      E h; E h; F h; G h; G h; F h; E h; D h; C h; C h; D h; E h; D h; C q; C t;
+      D h; D h; E h; C h; D h; E q; F q; E h; C h; D h; E q; F q; E h; D h; C h; D h; G t;
+      E h; E h; F h; G h; G h; F h; E h; D h; C h; C h; D h; E h; D h; C q; C t;};
+
+end
+
